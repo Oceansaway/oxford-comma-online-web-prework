19ad7df86581f560ab8de5630ddac2bb75702890
Stephanie Purple <spurple133@gmail.com>
2019-03-13 17:31:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length==2
   return array.join(" and ")
 elsif array.length>=3
-  array.insert(-2, " and")
+  
   return array.join(", ")
+  array.insert(-2, " and")
 end
 end
